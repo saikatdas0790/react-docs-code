f11fcd7a4bd549c6169eaf6a25587075b20c00c0
Saikat Das <saikatdas0790@gmail.com>
2019-08-27 18:52:50 +0530

    Setup initial app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router, Link } from "@reach/router";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Docs Worked out Code</h1>
+      <section>
+        <h2>Hooks</h2>
+
+        <ol>
+          <li></li>
+        </ol>
+      </section>
     </div>
   );
 }
